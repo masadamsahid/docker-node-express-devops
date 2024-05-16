a18bd932c4d619366ba8c46f155de670ed2ab715
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-05-16 07:23:29 +0700

    with another  db image (mongodb)

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose.connect(mongoURL)
+  .then(() => {
+    console.log("Success connect to DB");
+  }).catch((e) => {
+    console.log(e);
+    setTimeout(connectWithRetry, 5000);
+  });
+}
+
+connectWithRetry();
+
 app.get("/", (req, res) => {
   res.send("<h2>Hi There</h2>")
 });
